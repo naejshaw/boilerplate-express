2d2e1f55866022fc6dfc032bd04368c7a7ed5218
Jean Almeida <jeanjfra@gmail.com>
2024-11-04 13:46:16 +0000

    att

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World")
-
+absolutePath = __dirname + "/views/index.html"
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(absolutePath);
   });
 
 
