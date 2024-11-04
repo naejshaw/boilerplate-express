ba7ff56f3b1fc8b7625d6288907d236b3152b48e
Jean Almeida <jeanjfra@gmail.com>
2024-11-04 13:37:10 +0000

    add app.get

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
 
 console.log("Hello World")
+
+app.get("/", () => {
+    res.send("Hello Express")
+})
 
 
 
